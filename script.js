befdbe70c2a4145e7e0a1c8d5e6c57ffe8c448e1
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2023-02-04 16:16:50 +0100

    Sort by discount function finished

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
+/* - - - - - - - - LOAD THE PRODUCTS - - - - - - - - */
 
-//Ide töltődik be a productList tömb
 const productsContainer = document.querySelector(".products")
 
-//ez olvassa ki a option value-ját 0-tól 3-ig
-const dropdownListOption = document.querySelector(".custom-select > select > option").value
-
-// Betöltésnél létrehozza az alap listát
-function displayItems(array = []) {
-    productsContainer.innerHTML = ""
+function displayItems() {
 
     for (product of productList) {
         productsContainer.innerHTML += `
@@ -31,6 +26,7 @@ window.addEventListener("load", displayItems)
 
 
 /* - - - - - - - - DROPDOWN SORTING - - - - - - - - */
+const dropdownList = document.querySelector(".custom-select")
 
 function sortByPriceAscend(arr) {
     arr.sort((a, b) => a.currentPrice - b.currentPrice)
@@ -52,127 +48,64 @@ function sortByName(a, b) {
     return 0
 }
 
-
-
+// - Display products according to the dropdown sorting
 const dropdownValueChange = (e) => {
     productsContainer.innerHTML = ""
 
     if (e.target.value * 1 === 0) {
-
-        console.log("Ár szerint növekvő")
-
         displayItems(sortByPriceAscend(productList))
 
     } else if (e.target.value * 1 === 1) {
-
-        console.log("Ár szerint csökkenő")
-
         displayItems(sortByPriceDescend(productList))
 
     } else if (e.target.value * 1 === 2) {
-        console.log("ABC növekvő")
-
         displayItems(productList.sort(sortByName))
 
     } else if (e.target.value * 1 === 3) {
-        console.log("ABC csökkenő")
-
         displayItems(productList.sort(sortByName).reverse())
     }
 }
 
-
-// ez indítja el a dropdown lista elem értékének kiolvasását végző függvényt
-window.addEventListener("change", dropdownValueChange)
+dropdownList.addEventListener("change", dropdownValueChange)
 
 
 
+/* - - - - - - - - CHECKBOX FOR DISCOUNT PRODUCTS - - - - - - - - */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* const productCardComponent = (name, image, currentPrice, previousPrice) => {
-    return `
-    <div class="product">
-            <span class="image-container">
-                <img
-                    src="${image}">
-            </span>
-            <div class="product-data" data-identifier="6">
-                <div class="product-name">${name}</div>
-                <div class="product-price">${currentPrice.toLocaleString()} Ft</div>
-                <div class="product-old-price">${previousPrice.toLocaleString()} Ft</div>
-            </div>
-        </div>
-    `
-}
-
-const loadEvent = () => {
-    const rootElement = document.getElementById("root")
-
-    for (let product of products) {
-        rootElement.insertAdjacentHTML("beforeend", productCardComponent(product.name, product.image, product.currentPrice, product.previousPrice))
-    }
-}
-
-window.addEventListener("load", loadEvent)
-
-
-const checkBox = document.querySelector(".sale")
-const generatedItemList = document.querySelectorAll
+const checkBox = document.querySelector("input[type='checkbox']")
 
 
 const checkBoxCheck = () => {
+    productsContainer.innerHTML = ""
 
+    if (checkBox.checked === true) {
+
+        const discountProducts = productList.filter(product => product.previousPrice !== "")
+        console.log(discountProducts)
+
+        for (product of discountProducts) {
+            productsContainer.innerHTML += `
+            <div class="product">
+                <span class="image-container">
+                    <img
+                        src="${product.image}">
+                </span>
+                <div class="product-data" data-identifier="6">
+                    <div class="product-name">${product.name}</div>
+                    <div class="product-price">${product.currentPrice.toLocaleString()} Ft</div>
+                    <div class="product-old-price">${product.previousPrice.toLocaleString()} Ft</div>
+                </div>
+            </div>
+            `
+        }
+
+
+    } else {
+
+        displayItems(productList)
+
+    }
 
 }
 
-checkBox.addEventListener("click", checkBoxCheck)
-
-const clear = () => {
-    const rootElement = document.getElementById("root")
-    rootElement = []
-}
-
-window.addEventListener("click", clear) */
+checkBox.addEventListener("change", checkBoxCheck)
